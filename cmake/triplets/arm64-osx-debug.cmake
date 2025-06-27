854e2775a98d88c9a57618ed93cc532f67543c73
ccp-serpent <162123512+ccp-serpent@users.noreply.github.com>
2025-06-27 11:47:26 +0000

    Support custom build types

---
+++ cmake/triplets/arm64-osx-debug.cmake
@@ -0,0 +1,17 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_BUILD_TYPE "release")
+
+set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
+set(VCPKG_OSX_ARCHITECTURES arm64)
+
+set(CARBON_BUILD_TYPE "Debug")
+
+if (PORT MATCHES "openssl")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+if (PORT MATCHES "libuv")
+    set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DBUILD_TESTING=OFF")
+endif()
