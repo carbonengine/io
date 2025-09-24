5092266004e11d7284701be593c17a4218b4b349
CCP Toebeans <105929497+ccptoebeans@users.noreply.github.com>
2025-09-24 15:56:39 +0000

    always build the release version of openssl

--- cmake/triplets/universal-osx-internal.cmake
+++ cmake/triplets/universal-osx-internal.cmake
@@ -9,8 +9,8 @@ set(VCPKG_OSX_ARCHITECTURES "arm64;x86_64")
 set(CARBON_BUILD_TYPE "Internal")
 
 if (PORT MATCHES "openssl")
-    set(CARBON_x86_64_TRIPLET "arm64-osx-debug")
-    set(CARBON_arm64_TRIPLET "x64-osx-debug")
+    set(CARBON_x86_64_TRIPLET "x64-osx")
+    set(CARBON_arm64_TRIPLET "arm64-osx")
     set(VCPKG_LIBRARY_LINKAGE static)
 endif()
 
